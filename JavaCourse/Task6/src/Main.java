9a9bf897c03fb9c480abed5d60943ac934e9cc70
Violetta Tolkacheva <v.tolkacheva@21vek.by>
2023-08-28 21:42:17 +0300

    Task6 Classes

---
+++ JavaCourse/Task6/src/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+    public static void main(String[] args) {
+        //Задача 1
+        Fraction fraction = new Fraction(4, 5);
+
+        fraction.showFraction();
+        System.out.println(fraction.addFraction(7.8));
+        System.out.println(fraction.multiplyFraction(9.999));
+        System.out.println(fraction.divideFraction(4.4));
+
+        //Задача 2
+        Phone phone1 = new Phone("375259098678");
+        phone1.setModel("Samsung");
+        phone1.setWeight(178.4);
+
+        Phone phone2 = new Phone("375299098678", "Huawei", 190.2);
+
+        Phone phone3 = new Phone();
+        phone3.setNumber("375259092583");
+        phone3.setModel("iPhone");
+        phone3.setWeight(150);
+
+        System.out.println("Номер " + phone1.getNumber() + ", модель " + phone1.getModel() + ", вес " + phone1.getWeight());
+        System.out.println("Номер " + phone2.getNumber() + ", модель " + phone2.getModel() + ", вес " + phone2.getWeight());
+        System.out.println("Номер " + phone3.getNumber() + ", модель " + phone3.getModel() + ", вес " + phone3.getWeight());
+
+        phone1.receiveCall("Мария");
+        phone2.receiveCall("Петр", "375291970989");
+        phone3.sendMessage("375447896789", "375299007895", "375257896789", "375298978966");
+        phone1.sendMessage("375291963724");
+
+    }
+}
---
+++ JavaCourse/Task6/src/Phone.java
@@ -0,0 +1,56 @@
+public class Phone {
+    private String number;
+    private String model;
+    private double weight;
+
+    public Phone(String number, String model, double weight) {
+        this.number = number;
+        this.model = model;
+        this.weight = weight;
+    }
+    public Phone(String number) {
+        this.number = number;
+    }
+
+    public Phone() {
+    }
+
+    public void receiveCall (String name) {
+        System.out.println("Звонит " + name);
+    }
+    public void receiveCall (String name, String number) {
+        System.out.println("Звонит " + name + " " + number);
+    }
+
+    public void sendMessage (String ... numbers) {
+        System.out.println("Сообщение отправлено: ");
+        for(String i : numbers) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+}
